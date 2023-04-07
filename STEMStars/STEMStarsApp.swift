f4b2d2fdbbf53ad9de9c5213da521c5394fc9c0b
Marnie Reid <marnie@kodewithklossy.com>
2023-04-07 12:12:10 +0200

    Initial Commit

---
+++ STEMStars/STEMStarsApp.swift
@@ -0,0 +1,17 @@
+//
+//  STEMStarsApp.swift
+//  STEMStars
+//
+//  Created by scholar on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct STEMStarsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
